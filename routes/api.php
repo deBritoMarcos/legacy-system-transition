cff9c142b6aa77dca42b046b3de130cd361e2068
Marcos Brito <marcosbrito.dev@gmail.com>
2025-09-21 01:38:53 -0300

    refactor: corrige rota de cadastro na api

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,5 @@ use App\Http\Controllers\Api\Fornecedores\FornecedorResourceController;
 
 Route::prefix('/fornecedores')->name('fornecedores.')->group(function () {
     Route::get('/', [FornecedorResourceController::class, 'listar'])->name('listar');
-    Route::post('/salvar', [FornecedorResourceController::class, 'salvar'])->name('salvar');
+    Route::post('/', [FornecedorResourceController::class, 'salvar'])->name('salvar');
 });
